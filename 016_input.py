b5bbffd419629091fa180994caff3dcf2c3f7262
tuertolin <tuertolin@gmail.com>
2021-03-14 14:17:47 +1100

    Still studying

---
+++ 016_input.py
@@ -0,0 +1,10 @@
+name = input("Tell me your name please: ")
+print(f"\n Hello, {name}!")
+
+#Python input get's strings
+age = input(f"\n{name.title()}, how old are you?:\n")
+
+# Convert an string to INT
+age = int(age)
+age >= 18  
+
