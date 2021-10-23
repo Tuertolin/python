f02cc54c0fd16353a38044669dc1f8d34d1590a3
Gaston Gonzalez <gastogon@amazon.com>
2021-10-23 22:07:15 +1100

    Aliens fleet direction

--- 05_monkeys/alien_invasion.py
+++ 05_monkeys/alien_invasion.py
@@ -32,7 +32,6 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()           
-            self._update_aliens()
             self._update_screen()
 
     def _create_fleet(self):
@@ -65,19 +64,6 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
-    def _check_fleet_edges(self):
-        """Respond appropriately if any aliens have reached an edge."""
-        for alien in self.aliens.sprites():
-            if alien.check_edges():
-                self._change_fleet_direction()
-                break
-
-    def _change_fleet_direction(self):
-        """Drop the entire fleet and change the fleet's direction."""
-        for alien in self.aliens.sprites():
-            alien.rect.y += self.settings.fleet_drop_speed
-        self.settings.fleet_direction *= -1
-
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
         # Update bullet positions.
@@ -86,12 +72,6 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-
-    def _update_aliens(self):
-        """Update the position of all aliens in the fleet"""
-        self._check_fleet_edges()
-        self.aliens.update()
-
 
     def _check_events(self):
         # Watch for Keyboard and mouse events.
